da62bbb38390fd95cf057512f603a8f2eff69a20
bruc3 <bruce.mayiani@strathmore.edu>
2025-09-17 18:10:40 +0300

    phpmail comit

---
+++ Plugins/PHPMailer/mail.php
@@ -0,0 +1,38 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader (created by composer, not included with PHPMailer)
+require __DIR__  .  '/../../vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'bruce.mayiani@strathmore.edu';                     //SMTP username
+    $mail->Password   = 'fujo bxkm ldvu hclu';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('bruce.mayiani@strathmore.edu', 'Bruce Mayiani');
+    $mail->addAddress('mayianibruce@gmail.com', 'Klein Moreti');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Efootball Camp';
+    $mail->Body    = 'Welcome to Efootbball Training Gameplay.';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
---
+++ conf.php
@@ -0,0 +1,23 @@
+<?php
+// Site configuration
+$conf['site_name'] = "APP CREATION";
+$conf['site_email'] = "info@bbit.edu";
+$conf['site_url'] = "http://localhost/pro/dol";
+
+// Site language
+$conf['language'] = "en";
+
+// Database constants
+$conf['db_type'] = "pdo";
+$conf['db_host'] = "localhost";
+$conf['db_user'] = "root";
+$conf['db_pass'] = "bruce";
+$conf['db_name'] = "pro_db";
+
+// Email configuration
+$conf['mail_type'] = "smtp"; // Options: smtp, sendmail, mail
+$conf['smtp_host'] = "smtp.gmail.com";
+$conf['smtp_user'] = "bruce.mayiani@strathmore.edu";
+$conf['smtp_pass'] = "fujo bxkm ldvu hclu";
+$conf['smtp_port'] = 465;
+$conf['smtp_secure'] = "ssl";
